eff150a37476e300e8670caca6b659ad63159f26
xmayeur <xavier@mayeur.be>
2023-12-05 18:58:12 +0100

    Creating GUI... adding observer.org file input capture

--- o2eb.py
+++ o2eb.py
@@ -8,109 +8,103 @@
 """
 import tkinter as tk
 from tkinter import filedialog as fd
-from tkinter.messagebox import showinfo
+# from tkinter.messagebox import showinfo
 from tkinter import ttk
 from PIL import ImageTk, Image
+import os
 
 
-def select_file():
-    """
-    Selects one or more CSV files using a file dialog box.
+class O2ebGui(tk.Tk):
 
-    Returns:
-        None
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
 
-    """
-    filetypes = (('CSV file', '*.csv'),)
-    filenames = fd.askopenfiles(
-        title='Select files',
-        initialdir='./obs_data',
-        filetypes=filetypes
-    )
+        self.win_width = 1000
+        self.win_height = 400
+        self.filenames = []
 
-    showinfo(
-        title='Selected files',
-        message=', '.join([f.name for f in filenames])
-    )
+        self.title("Observation to eBird list conversion")
+        self.geometry(f"{self.win_width}x{self.win_height}")
 
+        # Define the frame
+        self.frm = ttk.Frame(self,
+                             width=self.win_width,
+                             height=self.win_height,
+                             padding=0)
+        self.frm.grid_propagate(True)
+        self.frm.grid()
 
-def define_root_windows(win_width=1000, win_height=400, background_image=None):
-    """
-    :param win_width: (int) The width of the root window. Default is 1000.
-    :param win_height: (int) The height of the root window. Default is 400.
-    :param background_image: (str) The path to the background image for the root window. Default is None.
-    :return: (tkinter.Tk) The root window.
-
-    This method creates and configures the root window for the Observation to eBird list conversion application.
-    The root window is the main window of the application.
-
-    Example usage:
-        root = define_root_windows(1200, 600, "images/background.jpg")
-        root.mainloop()
-    """
-    master = tk.Tk()
-    master.title("Observation to eBird list conversion")
-    master.geometry(f"{win_width}x{win_height}")
-
-    if background_image is not None:
-        _img = Image.open(background_image)
-        _factor = win_width / _img.width
-        new_h = int(_img.height * _factor)
-        new_w = win_width
-        _img = _img.resize((new_w, new_h), Image.LANCZOS)
-        _img = _img.crop((0, int(new_h * 0.25), new_w, int(new_h * 75)))
-        img = ImageTk.PhotoImage(_img)
-        lbl1 = tk.Label(image=img)
-        lbl1.image = img
-        lbl1.place(x=0, y=0)
-
-    return master
-
-
-def new_image_frame(root, image=None, **kwargs):
-    _frm = ttk.Frame(root, **kwargs)
-    _frm.grid_propagate(0)
-    _frm.grid()
-    w = kwargs['width']
-    if image is not None:
-        # open and resize the image
-        img = Image.open(image)
-        _factor = w / img.width
-        new_h = int(img.height * _factor)
-        img = img.resize((w, new_h), Image.LANCZOS)
-        img = ImageTk.PhotoImage(img)
-        lbl1 = ttk.Label(_frm, image=img)
-        lbl1.image = img
+        # set an image on the top of the frame
+        self.img = self.get_image('./images/background.jpg', (self.win_width, self.win_height), True)
+        self.lbl_img = ttk.Label(self.frm, image=self.img)
+        self.lbl_img.image = self.img
         # lbl1.place(x=0, y=0)
-        lbl1.grid(column=0, row=0)
-    return _frm
+        self.lbl_img.grid(column=0, row=0, columnspan=2)
+
+        # display the current directory
+        self.folder_txt = tk.StringVar()
+        self.folder_txt.set(f"Current folder: {os.path.abspath(os.getcwd())}")
+        self.lbl_folder = ttk.Label(self.frm, textvariable=self.folder_txt)
+        self.lbl_folder.grid(column=0, row=1, sticky=tk.W, padx=50, pady=10)
+        _img = self.get_image('./images/folder.png', (30, 30), True)
+
+        # define an input field to capture observation file to import
+        # val = self.register(self.check_files_exist)
+        self.input = tk.StringVar()
+        self.inp = ttk.Entry(self.frm, textvariable=self.input, justify=tk.LEFT, width=34)
+        self.inp.grid(column=0, row=2, sticky=tk.W, padx=50, pady=0)
+        self.inp.update()
+
+        # validatecommand=val('%P', validate='focusout')
+
+        # define a button to select files to import
+        self.btn_folder = ttk.Button(
+            self.frm,
+            width=0,
+            image=_img,
+            command=self.select_files
+        )
+        self.btn_folder.image = _img
+        self.btn_folder.grid(column=0, row=2, padx=self.inp.winfo_width()-40)
+
+        # self.btn_quit = ttk.Button(self.frm, text="Quit", command=self.destroy)
+        # self.btn_quit.grid(column=1, row=3)
+
+#    def check_files_exist(self, filepath):
+#        return True
+
+    @staticmethod
+    def get_image(image_path: str, size=tuple(), keep_proportion=True):
+        _img = Image.open(image_path)
+        w, h = size
+        factor = w / _img.width if keep_proportion else 1
+        _img = _img.resize((w, int(_img.height * factor)), Image.LANCZOS)
+        return ImageTk.PhotoImage(_img)
+
+    def select_files(self):
+        """
+        Selects one or more CSV files using a file dialog box.
+
+        Returns:
+            None
+
+        """
+        filetypes = (('CSV file', '*.csv'),)
+        self.filenames = fd.askopenfiles(
+            title='Select files',
+            initialdir='./obs_data',
+            filetypes=filetypes
+        )
+        _text = ', '.join([os.path.basename(x.name) for x in self.filenames])
+        self.folder_txt.set(f"Current folder: {os.path.dirname(self.filenames[0].name)}")
+        self.inp.delete(0, tk.END)
+        self.inp.insert(0, _text)
+
 
 
 def main():
-    win_width = 1000
-    win_height = 400
-    root = define_root_windows(win_width=win_width, win_height=win_height)
-    frm = new_image_frame(root=root,
-                    image='images/background.jpg',
-                    width=win_width,
-                    height=win_height,
-                    padding=10)
-    # frm = ttk.Frame(root, padding=10, width=win_width, height=200)
-    # frm.grid_propagate(0)
-    # frm.grid()
-
-    ttk.Label(frm, text="Hello World!").grid(column=0, row=1)
-    ttk.Button(
-        frm,
-        text='Open a File',
-        command=select_file
-    ).grid(column=0, row=2)
-
-    # open_button.pack(expand=True)
-
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=3)
-
-    root.mainloop()
+    gui = O2ebGui()
+    gui.mainloop()
 
 
 if __name__ == "__main__":
