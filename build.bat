cd7fb099dbf9b6d1d7db3ae3ae4733993c3532ef
xmayeur <xavier@mayeur.be>
2023-12-09 18:23:41 +0100

    Correct build pipeline and improve compatibility
    Added commands to remove `dist/o2eb/_internal` directory in `azure-pipelines.yml` for Linux and MacOS builds. This is to ensure the unwanted internal files and folders are removed post build. Additionally, path in `build.bat` was adjusted to use double quotes instead of single quotes for better compatibility across different shell environments. This resolves issues with file paths that includes spaces or special characters.

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
  venv\scripts\activate
  pyinstaller --onedir --windowed ^
- --add-data 'images\background.jpg:images' ^
- --add-data 'images\folder.png:images' ^
+ --add-data "images\background.jpg:images" ^
+ --add-data "images\folder.png:images" ^
  --noconfirm ^
  --icon images\bird.png ^
- --collect-submodules  'obs2ebird.py' ^
- --collect-submodules  'get_secrets.py' ^
- --hidden-import='PIL._tkinter_finder' ^
+ --collect-submodules  "obs2ebird.py" ^
+ --collect-submodules  "get_secrets.py" ^
+ --hidden-import="PIL._tkinter_finder" ^
  o2eb.py
